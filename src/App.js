41f8460f85ca87ef3607b51fb11e7bdcb4bb8d3a
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-23 08:36:13 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ class App extends Component {
           allows us to add in components, which are separate, self-contained chunks of JSX.
         </p>
         <ExampleComponent />
+        <TestComponent />
+          <Now />
 
       </div>
     );
